f69d8592460856085b1ccf5c00fa25ed6bb0503b
uthsa <uthsavi29@gmail.com>
2020-11-09 14:17:44 +0530

    Added:To Flip A Coin To Display Heads Or Tails

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 
 echo "Welcome To Flip Coin Combination"
+
+echo "Flip A Coin To Display Heads Or Tails"
+
+outCome=$((RANDOM%2))
+if [[ $outCome -eq 1 ]]
+then
+        echo "Head"
+else
+        echo "Tail"
+fi
